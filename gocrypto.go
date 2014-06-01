a3439df121a18a9cd49cc93e8f118b4f35bbe5b7
Ch6t9v0 <chase_schultz@gap.com>
2014-06-01 00:48:04 -0700

    Fixed Seal/Open Args & Print Formating

--- gocrypto.go
+++ gocrypto.go
@@ -19,29 +19,31 @@ func main() {
 	//Static 16 Byte Key Example
 	//key := []byte("example key 1234")
 
-	fmt.Printf("Whatup Doggy!  --  Let's Encyrpt some shit!\n\n\tUsing this key:  %s\n\n", base64.StdEncoding.EncodeToString(key))
+	fmt.Printf("Whatup Doggy!  --  Let's Encyrpt some shit!\n\n")
+	fmt.Printf("\t-----------------------------------------\n")
+	fmt.Printf("\tUsing this key:  %s\n\n", base64.StdEncoding.EncodeToString(key))
 
 	//Create an Instance of the AES Cipher & Wrap it with GCM
 	block, _ := aes.NewCipher(key)
 	gcmAES, _ := cipher.NewGCM(block)
-	fmt.Printf("The Size of the nonce...%d\n\n----------------\n\n", gcmAES.NonceSize())
+
+	fmt.Printf("\tNonce Size: %d\n", gcmAES.NonceSize())
+	fmt.Printf("\t-----------------------------------------\n\n")
 
 	plaintext := []byte("Punch it Chewy!")
 	fmt.Printf("Plaintext:\n\t%s\n\n", plaintext)
-	var ciphertext []byte
 
 	//Generate a Random Nonce
 	nonce := make([]byte, gcmAES.NonceSize())
 	rand.Read(nonce)
 
 	//Encrypt the plaintext data
-	ciphertext_data := gcmAES.Seal(ciphertext, nonce, plaintext, nil)
+	ciphertext_data := gcmAES.Seal(nil, nonce, plaintext, nil)
 
 	fmt.Printf("Ciphertext:\n\t%s\n\n", base64.StdEncoding.EncodeToString(ciphertext_data))
 
 	//Decrypt the ciphertext data
-	decryptedPlaintext := make([]byte, len(plaintext))
-	plaintext_data, _ := gcmAES.Open(decryptedPlaintext, nonce, ciphertext_data, nil)
+	plaintext_data, _ := gcmAES.Open(nil, nonce, ciphertext_data, nil)
 
 	//log.Println(err)
 	fmt.Printf("Decrypted Text:\n\t%s\n\n", plaintext_data)
